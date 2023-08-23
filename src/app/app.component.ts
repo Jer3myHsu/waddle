4323f4dc45ffaac0b8ad7d3b062df3fabc0b30ff
Jer3myHsu <34175005+Jer3myHsu@users.noreply.github.com>
2023-08-22 21:57:23 -0400

    Added keyboard component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'waddle';
+  // https://random-word-api.herokuapp.com/word?length=5&lang=en
+}
---
+++ src/app/components/keyboard/keyboard.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboard',
+  templateUrl: './keyboard.component.html',
+  styleUrls: ['./keyboard.component.scss']
+})
+export class KeyboardComponent {
+  keyboard = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Back']
+  ]
+
+  getKeyColumnSize(index: number, row: number, first: boolean, last: boolean) {
+    let left = (index * 2) + 1;
+
+    if (row > 0) {
+      left++;
+    }
+    let right = left + 2;
+
+    if (row === 2 && first) {
+      left--;
+    } else if (row === 2 && last) {
+      right++;
+    }
+    return `${left}/${right}`;
+  }
+}
